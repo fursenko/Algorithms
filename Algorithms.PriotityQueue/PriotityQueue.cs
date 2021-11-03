a24e9be6907e5d4f222ccd3368c0e749d648eb06
Volodymyr Fursenko <v.fursenko@gmail.com>
2021-11-03 12:54:34 +0200

    Add priority queue

---
+++ Algorithms.PriotityQueue/PriotityQueue.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Algorithms.PriotityQueue
+{
+    public class PriotityQueue<T> where T : IComparable
+    {
+        T[] _arr;
+        int I = 0;
+        public int Size { get { return I; } }
+
+        public PriotityQueue(int n)
+        {
+            _arr = new T[n + 1];
+        }
+
+        public void Insert(T vl)
+        {
+            _arr[++I] = vl;
+            Swim(I);
+        }
+
+        public T DelMax()
+        {
+            var max = _arr[1];
+            Swap(1, I);
+            _arr[I--] = default(T);
+            Sink(1);
+            return max;
+        }
+
+        void Swim(int k)
+        {
+            while (k > 1 && Less(k / 2, k))
+            {
+                Swap(k, k / 2);
+                k /= 2;
+            }
+        }
+
+        void Sink(int k) 
+        {
+            while (2*k <= I)
+            {
+                var j = 2 * k;
+                if (j < I && Less(j, j + 1)) j++;
+                if (!Less(k, j)) break;
+                Swap(k, j);
+                k = j;
+            }
+        }
+
+        void Swap(int i, int j)
+        {
+            var t = _arr[i];
+            _arr[i] = _arr[j];
+            _arr[j] = t;
+        }
+
+        bool Less(int i, int j)
+        {
+            return _arr[i].CompareTo(_arr[j]) < 0;
+        }
+    }
+}
---
+++ Algorithms.PriotityQueue/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Algorithms.PriotityQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = 10;
+            var random = new Random();
+            var pq = new PriotityQueue<int>(n);
+            for (int i = 0; i < n; i++)
+                pq.Insert(random.Next(1, n));
+
+            while (pq.Size > 0)
+            {
+                Console.Write($" {pq.DelMax()}");
+            }
+        }
+    }
+}
